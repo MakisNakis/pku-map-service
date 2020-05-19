c933612a4ac9267e61d0885f308eafde264e640c
2019_rk6_63_akhmedovre <rusteeck@icloud.com>
2020-05-19 20:20:48 +0300

    - Инициализация проекта на реакте - Создал Компонент карты - Сделал разовую отрисовку маркеров на карте, считав данные из файла в формате JSON (бывшая таблица CSV, импортированная из БД)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import MapComponent from './selfComponents/MapComponent';
+// import {Map as LeafletMap, Marker, Popup, TileLayer} from "react-leaflet";
+// import {Icon} from "leaflet";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import * as pkuData from "./data/tRouteTrackPointsKarabash.json"
+// console.log(pkuData);
+
+// class App extends Component {
+//
+//     render() {
+//         const loadingPosition = [54.730922, 55.962198];
+//
+//         return (
+//             <div>
+//                 {/*<div className="mainHeader"> <h1>Объекты для монтажа оборудования </h1></div>*/}
+//
+//
+//                 <Map/>
+//
+//
+//
+//
+//
+//             </div>
+//
+//         );
+//     }
+//
+// }
+
+export default function App() {
+    return (
+     <MapComponent/>
+
+    );
+
 }
+{/*<LeafletMap center = {[54.730922, 55.962198]} zoom = {12}>*/}
+{/*    <TileLayer*/}
+{/*        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"*/}
+{/*        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"*/}
+{/*    />*/}
 
-export default App;
+{/*</LeafletMap>*/}
---
+++ src/selfComponents/MapComponent.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {Map as LeafletMap, Marker, Popup, TileLayer} from "react-leaflet";
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
+import 'leaflet-defaulticon-compatibility';
+
+import {Icon} from "leaflet";
+import * as pkuData from "../data/tRouteTrackPointsKarabash.json"
+console.log(pkuData);
+
+class MapComponent extends React.Component {
+    render() {
+
+        return (
+            <div>
+            <LeafletMap center={[54.730922, 55.962198]} zoom={12}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+
+                {pkuData.pkuInfo.map(pku =>
+                    <Marker key ={pku.ID} position = {[pku.Latitude, pku.Longitude]}/>)}
+            </LeafletMap>
+            </div>
+        )
+    }
+}
+
+export default MapComponent;
