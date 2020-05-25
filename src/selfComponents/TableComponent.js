77e048fc5d7b2b53d6d9c51cfff91e298c220472
2019_rk6_63_akhmedovre <rusteeck@icloud.com>
2020-05-25 23:09:46 +0300

    - Внес некоторые визуальные изменения в таблицу

--- src/selfComponents/TableComponent.js
+++ src/selfComponents/TableComponent.js
@@ -3,7 +3,6 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import './css/TableComponent.css';
 import '../../node_modules/react-bootstrap-table/css/react-bootstrap-table.css';
 
-
 class TableComponent extends Component {
 
     constructor() {
@@ -49,70 +48,97 @@ class TableComponent extends Component {
         this.getData();
     }
 
+    //
     render() {
 
+
+
+        var selectRowProp = {
+            mode: "checkbox",
+            clickToSelect: true,
+            bgColor: "rgb(126,238,80)"
+
+        };
+
         const options = {
+            // size: "sm",
             page: 4,
             prePage:  '⟵',
             nextPage: '⟶',
             firstPage: '⟸',
             lastPage: '⟹',
-        }
+        };
 
         const cellEditProp = {
             mode: 'dbclick',
             // nonEditableRows: function () {      // не работает
             //     return [1];
             // }
-        }
+        };
+
         console.log(this.props.idPKU);
         return (
             <div id="TableComp">
                 {this.props.show &&
                     <div>
-                        <p className="Table-header">Basic Table</p>
+                        <p className="Table-header"><h2 align = "center">Перечень оборудования </h2></p>
                         <BootstrapTable data={this.state.pkuInfo}
+                                        exportCSV={ true }
+                                        table-reflow
                                         pagination={true}
                                         options={options}
                                         cellEdit={cellEditProp}
+                                        selectRow={selectRowProp}
+                                        striped
+                                        hover
+                                        condensed
+                                        insertRow
+                                        deleteRow
+                                        search
+                                        responsive
                         >
-                            <TableHeaderColumn isKey dataField='HardwareID'>
-                                HardwareID
+                            {/*<TableHeaderColumn isKey dataField='HardwareID'>*/}
+                            {/*    HardwareID*/}
+                            {/*</TableHeaderColumn>*/}
+                            <TableHeaderColumn isKey dataField='WorkName'  >
+                                Наименование работы
                             </TableHeaderColumn>
-                            <TableHeaderColumn dataField='WorkName'>
-                                WorkName
+                            {/*<TableHeaderColumn width = "380px" dataField='WorkType'>*/}
+                            {/*    Тип работы*/}
+                            {/*</TableHeaderColumn>*/}
+                            {/*<TableHeaderColumn dataField='WorkUnit'>*/}
+                            {/*    WorkUnit*/}
+                            {/*</TableHeaderColumn>*/}
+                            {/*<TableHeaderColumn dataField='WorkQuantity'>*/}
+                            {/*    WorkQuantity*/}
+                            {/*</TableHeaderColumn>*/}
+                            {/*<TableHeaderColumn dataField='WorkComment'>*/}
+                            {/*    WorkComment*/}
+                            {/*</TableHeaderColumn>*/}
+                            <TableHeaderColumn dataField='HardwareName' width = "50%" >
+                                Название оборудования
                             </TableHeaderColumn>
-                            <TableHeaderColumn dataField='WorkType'>
-                                WorkType
+                            <TableHeaderColumn dataField='HardwareQuantity'  width = "100px">
+                                Кол-во
                             </TableHeaderColumn>
-                            <TableHeaderColumn dataField='WorkUnit'>
-                                WorkUnit
-                            </TableHeaderColumn>
-                            <TableHeaderColumn dataField='WorkQuantity'>
-                                WorkQuantity
-                            </TableHeaderColumn>
-                            <TableHeaderColumn dataField='StartDate'>
-                                StartDate
-                            </TableHeaderColumn>
-                            <TableHeaderColumn dataField='EndDate'>
-                                EndDate
-                            </TableHeaderColumn>
-                            <TableHeaderColumn dataField='WorkComment'>
-                                WorkComment
-                            </TableHeaderColumn>
-                            <TableHeaderColumn dataField='HardwareName'>
-                                HardwareName
-                            </TableHeaderColumn>
-                            <TableHeaderColumn dataField='HardwareUnit'>
-                                HardwareUnit
-                            </TableHeaderColumn>
-                            <TableHeaderColumn dataField='HardwareQuantity'>
-                                HardwareQuantity
-                            </TableHeaderColumn>
-                            <TableHeaderColumn dataField='HardwareComment'>
-                                HardwareComment
+                            <TableHeaderColumn dataField='HardwareUnit' width = "100px">
+                                Ед.Изм.
                             </TableHeaderColumn>
 
+                            {/*<TableHeaderColumn dataField='HardwareComment'>*/}
+                            {/*    HardwareComment*/}
+                            {/*</TableHeaderColumn>*/}
+                            <TableHeaderColumn dataField='StartDate' width = "130px">
+                                Начало работ
+                            </TableHeaderColumn>
+
+                            <TableHeaderColumn dataField='EndDate' width = "130px">
+                                Конец работ
+                            </TableHeaderColumn>
+
+                            <TableHeaderColumn dataField='HardwareComment' width = "200px">
+                                Комментарий
+                            </TableHeaderColumn>
                         </BootstrapTable>
                     </div>
                 }
